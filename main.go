55da44ab30d1b18098345764a420fa0a33ce3aa1
Pedro Salgado <steenzout@users.noreply.github.com>
2017-05-18 19:22:03 -0600

    main: implemented web service.

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
-package api
+package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
